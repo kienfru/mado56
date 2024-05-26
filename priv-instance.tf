328bd8d03336d120859363eb9e18efe62e2304ef
kienfru <155900939+kienfru@users.noreply.github.com>
2024-05-26 20:22:38 +0200

    Update priv-instance.tf

--- priv-instance.tf
+++ priv-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "priv-server" {
     ami = var.my_ami
     instance_type = var.type
-    key_name = "clintontest"
+    key_name = "demo"
     subnet_id = aws_subnet.priv-sub.id
     security_groups = [ aws_security_group.kiensg.id ]
 
