7b77a028560221d04fc8fb95f78fb62cb05b2e28
kienfru <155900939+kienfru@users.noreply.github.com>
2024-05-26 20:57:05 +0200

    Update pub-instance.tf

--- pub-instance.tf
+++ pub-instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "pub-server1" {
     instance_type = var.type
     key_name = "demo"
     subnet_id = aws_subnet.pub-sub1.id
-    security_groups = [ aws_security_group.kiensg.id ]
+    security_groups = [ aws_security_group.kiensg1.id ]
 
     tags = {
       Name = "pub_server1"
